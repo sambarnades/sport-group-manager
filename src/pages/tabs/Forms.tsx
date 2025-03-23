c1997fbbd41824b3c6e41939e6d737f9ece0ec30
Samuel Barnades <samuel.barnades@gmail.com>
2025-03-23 10:33:23 +0100

    Improve form tab's sidemenu.

--- src/pages/tabs/Forms.tsx
+++ src/pages/tabs/Forms.tsx
@@ -1,18 +1,36 @@
 import React from "react";
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import { IonContent, IonPage, IonIcon, IonLabel } from "@ionic/react";
+
+import { addOutline, removeOutline } from "ionicons/icons";
 
 const Forms: React.FC = () => {
-    return (
-        <IonPage>
-            <IonContent fullscreen>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Forms</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-            </IonContent>
-        </IonPage>
-    );
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        {/* Start of content */}
+
+        {/* ------------------------- Sidebar ------------------------- */}
+
+        <div id="container" className="flex h-full">
+          <div className="basis-1/24 bg-slate-400">
+            <div className="grid grid-cols-1">
+              <div className="aspect-square h-10 bg-slate-600 m-2 rounded-xl flex justify-center items-center order-last  absolute bottom-15">
+                <IonIcon icon={addOutline} className="text-xl" />
+              </div>
+              <div className="aspect-square bg-slate-600 m-2 rounded-xl flex justify-center items-center">
+                <IonLabel>S1</IonLabel>
+              </div>
+            </div>
+          </div>
+          <div className="basis-23/24">
+            <p>Content</p>
+          </div>
+        </div>
+
+        {/* ------------------------------------------------------------------- */}
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Forms;
